0f1813562c3d31c65abecf388571c816aebb86f4
Charles-Henri Dupire <cdupire@gmail.com>
2020-04-17 21:14:04 +0200

    ajout de méthode création_liste_labyrinthe dans la classe Affichage

--- classes.py
+++ classes.py
@@ -1,6 +1,3 @@
-from constantes import COTE
-
-
 class Personnage:
     pass
 
@@ -17,37 +14,38 @@ class Gardien:
 
 
 class Affichage:
-    # Gestion de l'affichage du labyrinthe, des murs, du sol des objets et des personnages.
-    def __init__(self, abscisse_macgyver, ordonnee_macgyver):
-        self.abscisse_macgyver = abscisse_macgyver
-        self.ordonnee_macgyver = ordonnee_macgyver
+    """Gestion de l'affichage du labyrinthe, des murs, du sol des objets et des personnages."""
+    def __init__(self, coordonnees_macgyver):
+        """coordonnees_macgyver est un tuple (abscisse, ordonnee) avec pour origine le coin supérieur gauche"""
+        self.coordonnees_macgyver = coordonnees_macgyver
 
     def creation_liste_labyrinthe(self):
-        """création d'une liste de liste pour représenter le labyrinthe"""
-        liste_labyrinthe = [COTE][COTE]
+        """création d'une liste de chaine de caractères pour représenter le labyrinthe"""
+        liste_labyrinthe = []
         fichier_labyrinthe = open("labyrinthe.txt", "r")
-        lignes = fichier_labyrinthe.readlines()
-        for numero_ligne in range(0, COTE):
-            for numero_colonne in range(0, COTE):
+        ligne = fichier_labyrinthe.readline()
+        liste_ligne = list(ligne)
+        liste_labyrinthe.append(liste_ligne)
+        while ligne:
+            ligne = fichier_labyrinthe.readline()
+            liste_ligne = list(ligne)
+            liste_labyrinthe.append(liste_ligne)
+        fichier_labyrinthe.close()
+        return liste_labyrinthe
+
+    def ajout_position_macgyver(self, liste):
+        """modifie la liste afin d'ajouter la position de macgyver désigné par 'H' """
+        ligne = liste[self.coordonnees_macgyver[1]]
+        caractere = ligne[self.coordonnees_macgyver[0]]
+        if caractere == ' ':
+            ligne[self.coordonnees_macgyver[0]] = 'H'
+        return liste
+
+    def affichage_labyrinthe(self, liste):
+        """"affiche le labyrinthe à partir d'une liste de chaines de caractère"""
+        for ligne in liste:
             for carac in ligne:
-
-
-
-
-#def afficher_labyrinthe(self):
-     #   fichier_labyrinthe = open("labyrinthe.txt", "r")
-      #  lignes = fichier_labyrinthe.readlines()
-       # for countY in range(0, len(lignes)):
-        #    if countY == self.ordonnee_macgyver:
-                # lire la ligne caractère par caractère
-         #       for countX in range(0, len(lignes[countY])):
-          #         if countX == self.abscisse_macgyver:
-           #             print('G', end='')
-           #         else:
-            #            print(lignes[countY][countX], end='')
-           # else:
-            #    print(lignes[countY], end='')
-        # fichier_labyrinthe.close()
+                print(carac, end='')
 
 
 class GameManager:
--- main.py
+++ main.py
@@ -5,15 +5,15 @@ import classes
 
 
 def main():
-    affichage = classes.Affichage(13, 1)
-    affichage.afficher_labyrinthe()
-    while True:
-        choix = input("Entrez 'z':haut/'q':gauche/'s':bas/'d':droite  'fin' pour quitter")
-        if choix == 'q':
-            break
-        if choix == 'z':
-
-
+    affichage = classes.Affichage((13, 1))
+    liste = affichage.creation_liste_labyrinthe()
+    liste_macgyver = affichage.ajout_position_macgyver(liste)
+    affichage.affichage_labyrinthe(liste_macgyver)
+    #while True:
+     #   choix = input("Entrez 'z':haut/'q':gauche/'s':bas/'d':droite  'fin' pour quitter")
+      #  if choix == 'q':
+       #     break
+        #if choix == 'z':
 
 
 if __name__ == "__main__":
